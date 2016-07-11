b8ec1c39af22f162e885651da9b8dffecca6f114
double-qiu <934590736@qq.com>
2016-07-11 16:29:05 +0800

    init

---
+++ qtt-platform/src/test/java/com/ufdouble/QttPlatformApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ufdouble;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QttPlatformApplication.class)
+@WebAppConfiguration
+public class QttPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
